5060b2414dfd854a0f3b2b14b9d16f3e0c5bc932
Anton <tonystoyz@yandex.ru>
2022-07-16 22:38:54 +0300

    добавлена база, postman 99/99

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
@@ -21,5 +22,18 @@ public class Genre {
     @JsonCreator
     public static Genre forObject(@JsonProperty("id") int id, @JsonProperty String title) {
         return new Genre(id, title);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Genre genre = (Genre) o;
+        return getId().equals(genre.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
